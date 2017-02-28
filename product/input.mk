7400bd9302bb9b17189f929aaadca5cb5af73556
Jaekyun Seok <jaekyun@google.com>
2017-02-28 18:08:32 +0900

    product: Move input device config files into vendor partition
    Change-Id: I848718a8118d88da1f570575c3243bc072a6c6e1

--- product/input.mk
+++ product/input.mk
@@ -1,10 +1,10 @@
 # Keys layouts
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/apq8064-tabla-snd-card_Button_Jack.kl:system/usr/keylayout/apq8064-tabla-snd-card_Button_Jack.kl \
-    $(COMMON_PATH)/configs/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(COMMON_PATH)/configs/keypad_8960.kl:system/usr/keylayout/keypad_8960.kl \
-    $(COMMON_PATH)/configs/mhl-rcp.kl:system/usr/keylayout/mhl-rcp.kl \
-    $(COMMON_PATH)/configs/pmic8xxx_pwrkey.kl:system/usr/keylayout/pmic8xxx_pwrkey.kl
+    $(COMMON_PATH)/configs/apq8064-tabla-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/apq8064-tabla-snd-card_Button_Jack.kl \
+    $(COMMON_PATH)/configs/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(COMMON_PATH)/configs/keypad_8960.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/keypad_8960.kl \
+    $(COMMON_PATH)/configs/mhl-rcp.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/mhl-rcp.kl \
+    $(COMMON_PATH)/configs/pmic8xxx_pwrkey.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/pmic8xxx_pwrkey.kl
 
 # Touchscreen properties
 PRODUCT_PROPERTY_OVERRIDES += \
