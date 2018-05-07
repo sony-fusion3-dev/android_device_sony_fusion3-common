f854ea7ecbc7623cb3e02310a21de6a7826fb432
Bruno Martins <bgcngm@gmail.com>
2018-05-07 09:23:59 +0200

    camera: Remove obsolete legacy HAL1 props
     * Only TARGET_HAS_LEGACY_CAMERA_HAL1 board flag is needed now.
    
    Change-Id: I266445e7fd8bc84337d56333c8e3d3f536039fd1

--- product/camera.mk
+++ product/camera.mk
@@ -11,8 +11,3 @@ PRODUCT_PACKAGES += \
 # Camera configurations
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
-
-# Camera properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true
