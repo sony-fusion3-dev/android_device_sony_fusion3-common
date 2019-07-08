cb7f24e6ce8bf180799ea1066f437644ea12ada5
Amit Pundir <amit.pundir@linaro.org>
2019-07-08 22:17:09 +0530

    fusion3-common: Add gatekeeper software HIDL service.
    Use the default software implementation of gatekeeper.
    
    Change-Id: Id696752ad78047155cad6a5dafe7ca1b4fe86345
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    (cherry picked from commit a7514b20dd00cf303e957904a3f2c14b191fc4b5)

--- product/qcom-keymaster.mk
+++ product/qcom-keymaster.mk
@@ -1,3 +1,7 @@
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service.software
+
 # Keymaster HIDL interfaces
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
