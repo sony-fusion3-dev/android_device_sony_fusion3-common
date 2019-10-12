46c251ab6026da4ef531303e9d09e4d23720f90a
dianlujitao <dianlujitao@lineageos.org>
2019-10-12 10:12:37 +0800

    fusion3-common: Enable AOSP bluetooth audio HAL v2
    Change-Id: I4d12752ae5fb2829d6612647c8740c84967f5434

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -20,4 +20,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default
