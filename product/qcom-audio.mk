51a19daead25478bfafc92eef6a5bce303e23612
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    fusion3-common: Switch to Audio HAL V6
    Switch to Audio HAL V6
    
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test on taimen and walleye
    
    Change-Id: Ieb1cc5c7fdcc5416a328a0a5c4a1996258b81ac8

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -19,5 +19,6 @@ PRODUCT_PACKAGES += \
 
 # Audio HIDL interfaces
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl
+    android.hardware.audio@2.0-service \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl
