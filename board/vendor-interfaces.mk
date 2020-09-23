3f668f9f91e6f060ba94b04354316b25c922596b
Łukasz Patron <priv.luk@gmail.com>
2020-09-23 05:38:33 +0000

    fusion3-common: Set PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
    * Apparently this is now required on all non full treble devices since
      google added a sleep(1) in libhidl...

--- board/vendor-interfaces.mk
+++ board/vendor-interfaces.mk
@@ -1,3 +1,4 @@
 # Vendor Interface Manifest
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
