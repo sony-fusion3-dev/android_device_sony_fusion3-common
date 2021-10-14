4ed0ed3ccb44ee9d8e1c2d77ce9576affcbff72e
Kyle Harrison <khwebmail@gmail.com>
2021-10-14 10:00:52 +0100

    fusion3-common: Use threaded GLES render engine

--- product/display.mk
+++ product/display.mk
@@ -3,4 +3,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
     persist.hwc.mdpcomp.enable=true \
     persist.sys.wfd.virtual=0 \
-    debug.hwui.use_buffer_age=false
+    debug.hwui.use_buffer_age=false \
+    debug.renderengine.backend=threaded
