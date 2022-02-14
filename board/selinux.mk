217d31c0c9ece1599b030f67c5a84d557c6cfea7
TARKZiM <tom8476oo@gmail.com>
2022-02-14 11:42:04 +0800

    [DNM] fusion3-common: Bring up for S

--- board/selinux.mk
+++ board/selinux.mk
@@ -1,6 +1,8 @@
 # Common sepolicies
-BOARD_SEPOLICY_DIRS += \
-    $(COMMON_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#    $(COMMON_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy-minimal
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # LineageOS legacy sepolicies
-include device/qcom/sepolicy-legacy/sepolicy.mk
+#include device/qcom/sepolicy-legacy/sepolicy.mk
