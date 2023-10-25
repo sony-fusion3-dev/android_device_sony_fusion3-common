37bd6acd728468d04acf0e88702394a9a38241a8
TARKZiM <tom8476oo@gmail.com>
2023-10-25 23:24:37 +0800

    fusion3-common: Split radio manifests

--- board/vendor-interfaces.mk
+++ board/vendor-interfaces.mk
@@ -2,3 +2,7 @@
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
+
+ifneq ($(BOARD_HAVE_RADIO),false)
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_radio.xml
+endif
