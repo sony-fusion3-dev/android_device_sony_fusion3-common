7ea0006c7f092c0998915d799c50eae9ce206630
TARKZiM <tom8476oo@gmail.com>
2024-01-13 00:02:01 +0800

    fusion3-common: devicesettings: Update for U

--- devicesettings/Android.mk
+++ devicesettings/Android.mk
@@ -18,6 +18,8 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     $(TOP)/packages/resources/devicesettings/res
